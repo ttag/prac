7ee06f3b7d47473a3c8f2d92b7448e7f38f9d928
xeshan <xeshan@zeshan-desktop.(none)>
2010-10-19 15:14:59 +0600

    airport scaffold

---
+++ db/migrate/20101019090745_create_airports.rb
@@ -0,0 +1,13 @@
+class CreateAirports < ActiveRecord::Migration
+  def self.up
+    create_table :airports do |t|
+      t.string :code
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :airports
+  end
+end
