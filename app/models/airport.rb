2884ee08bbd7ec22f39d6b31c632ee8d99e86a69
xeshan <xeshan@zeshan-desktop.(none)>
2010-10-19 16:42:00 +0600

    - airport relation with flights - dropdown added

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,6 @@
 class Airport < ActiveRecord::Base
+
+  has_many :departures, :class_name => "Flight", :foreign_key => "origin_id"
+  has_many :arrivals, :class_name => "Flight", :foreign_key => "destination_id"
+
 end
